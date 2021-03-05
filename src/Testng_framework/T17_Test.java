e2be110f412dd8be5f177315a79f0dc109f88ae0
123 <123@DESKTOP-F5B8GGK>
2021-03-05 21:46:06 +0530

    assign 4

--- src/Testng_framework/T17_Test.java
+++ src/Testng_framework/T17_Test.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 
 import java.util.List;
 
-import org.junit.Assert;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,41 +23,35 @@ public class T17_Test {
 	//id of whole table
 		WebElement tbl=driver.findElement(By.xpath("/html/body/table/tbody/tr[3]/td[2]/table[1]"));
 			
+		
 		//fetch all tr of table
 		List<WebElement> rows=tbl.findElements(By.tagName("tr"));
 			
-		for(int i=3;i<=11;i++)
+		for(int i=3;i<15;i++)
 			{
 				
 				List<WebElement> cols=rows.get(i).findElements(By.tagName("td"));
-				Double pin=Double.valueOf(cols.get(2).getText());
+				double pin=Double.valueOf(cols.get(2).getText());
 				System.out.println("pincode 1: "+pin+" "+i);
 				
-				for(int j=i+1;j<=11;j++)
+				for(int j=i+1;j<15;j++)
 				{
 					List<WebElement> cols2=rows.get(j).findElements(By.tagName("td"));
+				
+				    System.out.println("pincode 2: "+cols2.get(2).getText()+" "+j);
+				
+				
+				    if(pin==(Double.valueOf(cols2.get(2).getText())))
+				    {
 					
-				
-				System.out.println("pincode 2: "+cols2.get(2).getText()+" "+j);
-				
-				
-				//System.out.println(pin);
-				if(pin==(Double.valueOf(cols2.get(2).getText())))
-				{
+					softAssertion.assertTrue(pin==(Double.valueOf(cols2.get(2).getText())));
+				 	System.out.println("pincode matched at row "+i+" and "+j);
 					
-					softAssertion.assertFalse(pin==(Double.valueOf(cols2.get(2).getText())));
-				  // System.out.print("Pincode Matched"+cols.get(2).getText()+"at number"+cols2.get(0).getText());
-					System.out.println("pincode row"+i+" col"+j);
-					
-				}
-				
+				    }
 				
 				}
 			}
 		softAssertion.assertAll();
-	  
-			   //System.out.println("no matched pincode ");
-		   
 	  
  }
   
